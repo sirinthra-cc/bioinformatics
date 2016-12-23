33bac7ac920d725b5d1c66b3f37ceedb8014cc98
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2016-12-23 10:55:15 +0700

    edit phenotype form

--- phenotype_influencing/forms.py
+++ phenotype_influencing/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class PhenotypeForm(forms.Form):
     input = forms.CharField(label="Input file")
-    output_path = forms.CharField(label="Output path")
+    # output_path = forms.CharField(label="Output path")
     output_name = forms.CharField(label='Output name', max_length=100)
