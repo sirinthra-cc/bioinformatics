9d0ae1f1f41facbfcd91dcf1604213de075b97ff
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-01-07 21:04:33 +0700

    import junior-sama's function

---
+++ tools/ExtractINFO_And_SpecifyGenes/extract.py
@@ -0,0 +1,164 @@
+import csv
+import codecs
+import time
+from collections import defaultdict
+#from sortedcontainers import SortedDict
+
+
+CHROM = 0; POS = 1; ID = 2; REF = 3; ALT = 4; QUAL = 5; FILTER = 6; INFO = 7; FORMAT = 8; G = 9
+CHR = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+       '20', '21', '22', 'X', 'Y']
+HEADER = ['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT', 'G']
+
+
+
+def extract_info(filename):
+    f = open(filename, 'r')
+
+    try:
+        reader = csv.reader(f)
+        read_en = False  # creates the reader object
+        add_col = defaultdict(list)
+        for row in reader:  # iterates the rows of the file in orders
+            # words = row.strip().split()
+            if read_en:
+                sep_info = row[7].split(";")
+                for s in sep_info:
+                    sep_s = s.split("=")
+                    if(len(sep_s) == 2):
+                        add_col[sep_s[0]].append(sep_s[1])
+            if row[0] == "#CHROM":
+                read_en = True
+                print("Found #CHROM")
+    finally:
+        f.close()  # closing
+    return add_col
+
+
+def get_variant_and_count(filename):
+    read_en = False
+    var_count = 0
+    variant = dict()
+    for chro in CHR:
+        variant[chro] = set()
+    for row in list(open(filename, "rb")):
+        words = row.strip().split()
+        if read_en and words[QUAL] != '.':
+            chro = words[CHROM][3:]
+            variant[chro].add(int(words[POS]))
+            var_count += 1
+        if words[CHROM] == "#CHROM":
+            read_en = True
+    return variant, var_count
+
+def extract_and_write(inputfile, outputfile):
+    rf = open(inputfile, 'r')
+    wf = open(outputfile, 'w',newline='')
+    #key_list = add_column.keys()
+
+    try:
+        writer = csv.writer(wf)
+        reader = csv.reader(rf)
+        read_en = False
+        for row in reader:  # iterates the rows of the file in orders
+            # words = row.strip().split()
+            if read_en:
+                #for value in add_column:
+                #for key in key_list:
+                #    row.append(add_column[key][count])
+                #writer.writerows(row)
+                #count = count + 1
+                sep_info = row[7].split(";")
+                add_info = []
+                count = 0;
+                for s in sep_info:
+                    sep_s = s.split("=")
+                    if count < 10:
+                        if (len(sep_s) == 2):
+                            add_info.append(sep_s[1])
+                        else : add_info.append(s)
+                    count = count + 1
+                writer.writerow(row + add_info)
+            elif row[0] == "#CHROM":
+                read_en = True
+                #iter_row = iter(reader)
+                row2 = next(reader)
+                sep = row2[7].split(";")
+                add_header = []
+                count2 = 0
+                for s in sep:
+                    sep_s = s.split("=")
+                    if count2 < 10:
+                        if (len(sep_s) == 2):
+                            add_header.append(sep_s[0])
+                    count2 = count2+1
+                writer.writerow(row + add_header)
+                print("Found #CHROM")
+            else :
+                writer.writerow(row)
+    finally:
+        rf.close()
+        wf.close()# closing
+
+def extract_and_write2(inputfile, outputfile):
+    rf = open(inputfile, 'r')
+    wf = open(outputfile, 'w',newline='')
+    #key_list = add_column.keys()
+
+    try:
+        #writer = csv.writer(wf)
+        reader = rf.readlines()
+        read_en = False
+        at_header = False
+        for line in reader:  # iterates the rows of the file in orders
+            # words = row.strip().split()
+            row = line.split('\t')
+            row[len(row)-1] = (row[len(row)-1].split('\n'))[0]
+            #row[len(row) - 1].strip('\n')
+            if read_en:
+                #for value in add_column:
+                #for key in key_list:
+                #    row.append(add_column[key][count])
+                #writer.writerows(row)
+                #count = count + 1
+                sep_info = row[7].split(";")
+                add_info = []
+                count = 0;
+                for s in sep_info:
+                    sep_s = s.split("=")
+                    if count < 9:
+                        if (len(sep_s) == 2):
+                            add_info.append(sep_s[1])
+                        else : add_info.append(s)
+                    count = count + 1
+                wf.write('\t'.join(row + add_info))
+                wf.write('\n')
+            elif row[0] == "#CHROM":
+                read_en = True
+                #sep = row[7].split(";")
+                #print(row[7])
+                #print(sep)
+                #add_header = []
+                #count2 = 0
+                #for s in sep:
+                #    sep_s = s.split("=")
+                #    if count2 < 9:
+                #        if (len(sep_s) == 2):
+                #            add_header.append(sep_s[0])
+                #    count2 = count2 + 1
+                #print(add_header)
+                add_header = ['AC1', 'AF1', 'DP', 'DP4', 'EXOMISER_GENE', 'EXOMISER_GENE_COMBINED_SCORE', 'EXOMISER_GENE_PHENO_SCORE', 'EXOMISER_GENE_VARIANT_SCORE', 'EXOMISER_VARIANT_SCORE']
+                #print(row+add_header)
+                wf.write('\t'.join(row + add_header))
+                wf.write('\n')
+                #at_header = False
+                #print("Found #CHROM")
+                #iter_row = iter(reader)
+
+
+            else :
+                wf.write('\t'.join(row))
+                wf.write('\n')
+    finally:
+        rf.close()
+        wf.close()# closing
---
+++ tools/ExtractINFO_And_SpecifyGenes/function.py
@@ -0,0 +1,87 @@
+import csv
+
+def getEntrez(ref_file,Gene_list_file,output_file):
+    reff = open(ref_file, 'r')
+    gf = open(Gene_list_file, 'r')
+    wf = open(output_file, 'w', newline='')
+
+    try :
+        ref_reader = csv.reader(reff)
+        gl_reader = csv.reader(gf)
+        writer = csv.writer(wf)
+
+        read_en = False
+        for row in ref_reader:
+            if read_en:
+
+                found_gene = False
+                add_info = []
+                gf.seek(0)
+                for row2 in gl_reader:
+                    if row[15] == row2[0]:
+                        add_info.append(row2[3])
+                        found_gene = True
+
+                if not found_gene:
+                    add_info.append("novel")
+
+                writer.writerow(row + add_info)
+            elif row[0] == "#CHROM":
+                read_en = True
+                # iter_row = iter(reader)
+                add_header = ["gene_type"]
+                writer.writerow(row + add_header)
+                print("Found #CHROM")
+            else:
+                writer.writerow(row)
+    finally:
+        reff.close()
+        gf.close()
+        wf.close()
+
+def specify_gene(ref_file,Gene_list_file,output_file):
+    reff = open(ref_file, 'r')
+    gf = open(Gene_list_file, 'r')
+    wf = open(output_file, 'w', newline='')
+
+    try:
+        ref_reader = reff.readlines()
+        gl_reader = csv.reader(gf)
+        #writer = csv.writer(wf)
+
+        read_en = False
+        for line in ref_reader:
+            row = line.split('\t')
+            row[len(row) - 1] = (row[len(row) - 1].split('\n'))[0]
+            #row[len(row)-1].strip('\n')
+            if read_en:
+
+                found_gene = False
+                add_info = []
+                gf.seek(0)
+                for row2 in gl_reader:
+                    if row[14] == row2[0]:
+                        add_info.append(row2[3])
+                        found_gene = True
+
+                if not found_gene:
+                    add_info.append("novel")
+
+                wf.writelines('\t'.join(row + add_info))
+                wf.write('\n')
+                #writer.writerow(row + add_info)
+            elif row[0] == "#CHROM":
+                read_en = True
+                # iter_row = iter(reader)
+                add_header = ["gene_type"]
+                wf.writelines('\t'.join(row + add_header))
+                wf.write('\n')
+                #writer.writerow(row + add_header)
+                #print("Found #CHROM")
+            else:
+                wf.writelines('\t'.join(row))
+                wf.write('\n')
+    finally:
+        reff.close()
+        gf.close()
+        wf.close()
