08857253b2c0e245cab974d1fab021eeed9a1dd9
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-01-18 18:02:30 +0700

    add get dp in output

--- commons/output_common.py
+++ commons/output_common.py
@@ -2,7 +2,7 @@ import os
 from tools.ExtractINFO_And_SpecifyGenes.export import export_specify_gene
 
 
-class ExomiserOutput:
+class Output:
     chrom = None
     pos = None
     ref = None
@@ -20,6 +20,17 @@ class ExomiserOutput:
 CHROM = 0; POS = 1; ID = 2; REF = 3; ALT = 4; QUAL = 5; FILTER = 6; INFO = 7; FORMAT = 8; G = 9
 
 
+def get_dp(info, g):
+    start = info.find('DP=')
+    if start != -1:
+        start += 3
+        end = info.find(';', start)
+    else:
+        start = g.find(':') + 1
+        end = g.find(':', start)
+    return int(info[start:end])
+
+
 def get_output_list(output_name):
     output_list = []
     read_en = False
@@ -29,15 +40,14 @@ def get_output_list(output_name):
     for row in list(open(file_path, "r")):
         words = row.strip().split()
         if read_en:
-            hiphive_output = ExomiserOutput(chrom=words[CHROM],
-                                            pos=words[POS],
-                                            ref=words[REF],
-                                            alt=words[ALT],
-                                            dp=words[DP],
-                                            )
-            output_list.append(hiphive_output)
+            output = Output(chrom=words[CHROM],
+                            pos=words[POS],
+                            ref=words[REF],
+                            alt=words[ALT],
+                            dp=get_dp(words[INFO], words[G]),
+                            )
+            output_list.append(output)
         if words[CHROM] == "#CHROM":
-            DP = words.index("DP", 9)
             read_en = True
 
     return output_list
