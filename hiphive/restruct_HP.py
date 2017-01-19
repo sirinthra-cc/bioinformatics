69e7ccbc92eae9a0d5586baa9b8adea27fb418c4
Jijy Thamjarat <jijy.chavisa@gmail.com>
2017-01-19 15:16:03 +0700

    change searching algorithm

--- hiphive/restruct_HP.py
+++ hiphive/restruct_HP.py
@@ -9,20 +9,46 @@ def hp_id_search(keyword):
             data[id_temp] = []
 
         elif lines[i].find("name", 0, 4) == 0:
-            data[id_temp].append(lines[i][6:].strip())
+            data[id_temp].append([lines[i][6:].strip(),"name"])
+            #print(id_temp,data[id_temp])
         elif lines[i].find("def", 0, 3) == 0:
-            data[id_temp].append(lines[i][6:].strip())
+            data[id_temp].append([lines[i][6:].strip(),"def"])
+            #print(id_temp,data[id_temp])
+
         elif lines[i].find("alt_id", 0, 6) == 0:
-            data[id_temp].append(lines[i][9:].strip())
+            data[id_temp].append([lines[i][9:].strip(),"alt_id"])
+            #print(id_temp,data[id_temp])
+
         elif lines[i].find("xref", 0, 4) == 0:
-            data[id_temp].append(lines[i][7:].strip())
+            data[id_temp].append([lines[i][7:].strip(),"xref"])
+            #print(id_temp,data[id_temp])
 
     result = []
 
     for x in data:
         for y in data[x]:
-            if y.find(keyword, 0, len(y)) > 0:
-                result.append(("HP:"+x, data[x][0]))
+            if y[0].find(keyword, 0, len(y[0])) > 0 and y[1] == "name":
+                result.append(("HP:"+x, data[x][0][0]))
+                data[x] = []
+               # print("append name")
                 break
-
+    for x in data:
+        for y in data[x]:
+            if y[0].find(keyword, 0, len(y[0])) > 0 and y[1] == "def":
+                result.append(("HP:"+x, data[x][0][0]))
+                data[x] = []
+              #  print("append def")
+                break
+    for x in data:
+        for y in data[x]:
+            if y[0].find(keyword, 0, len(y[0])) > 0 and y[1] == "alt_id":
+                data[x] = []
+               # print("append alt_id")
+                break
+    for x in data:
+        for y in data[x]:
+            if y[0].find(keyword, 0, len(y[0])) > 0 and y[1] == "xref":
+                data[x] = []
+              #  print("append xref")
+                break
     return result
