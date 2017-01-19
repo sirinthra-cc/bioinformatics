daa59aa6fb6f7c2d285f7bbf704fd4aa2f28981d
Pitchayut <pitchayut.jr@hotmail.com>
2017-01-19 12:20:40 +0700

    Specify from gene list

--- tools/ExtractINFO_And_SpecifyGenes/export.py
+++ tools/ExtractINFO_And_SpecifyGenes/export.py
@@ -11,10 +11,10 @@ gl_file = BASE_DIR + "/tools/ExtractINFO_And_SpecifyGenes/Gene_List.csv"
 # getEntrez(ref_file,gl_file,output_file)
 
 
-def export_specify_gene(output_name):
+def export_specify_gene(output_name,target_list,candidate_list):
     input_path = BASE_DIR + "/output/" + output_name + ".vcf"
     extract_info_out = BASE_DIR + "/output/" + output_name + "-extract-info.vcf"
     output_path = BASE_DIR + "/output/" + output_name + "-specify-gene.vcf"
 
     extract_and_write2(input_path, extract_info_out)
-    specify_gene(extract_info_out, gl_file, output_path)
+    specify_gene(extract_info_out, target_list, candidate_list, output_path)
