ec3966f04e87815be001a88c8909490277b1a2b0
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-01-19 19:03:34 +0700

    finish de novo

--- de_novo/de_novo.py
+++ de_novo/de_novo.py
@@ -1,5 +1,5 @@
-# from PyVCF import vcf
 import csv
+from _csv import QUOTE_NONE
 from sortedcontainers import SortedDict
 
 from config.settings import BASE_DIR
@@ -7,48 +7,50 @@ from config.settings import BASE_DIR
 CHROM = 0; POS = 1; ID = 2; REF = 3; ALT = 4; QUAL = 5; FILTER = 6; INFO = 7; FORMAT = 8; G = 9
 CHR = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
        '20', '21', '22', 'X', 'Y']
-HEADER = ['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT', 'G', 'C_DP']
+HEADER = ['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT', 'G']
 
 
-def de_novo(children_files, mother_file=None, father_file=None, output_name="filtered"):
+def de_novo(children_files, output_name, mother_file=None, father_file=None):
 
+    header = []
     de_novo = dict()
     sorted_m_depth = dict()
     sorted_f_depth = dict()
 
     if mother_file:
-        HEADER.append(get_name(mother_file) + '_DP')
+        header.append(get_name(mother_file) + '_DP')
         m_variant, m_depth = get_variant_and_depth(mother_file)
         for chro in CHR:
             sorted_m_depth[chro] = SortedDict((key, value) for key, value in m_depth[chro].items())
     if father_file:
-        HEADER.append(get_name(father_file) + '_DP')
+        header.append(get_name(father_file) + '_DP')
         f_variant, f_depth = get_variant_and_depth(father_file)
         for chro in CHR:
             sorted_f_depth[chro] = SortedDict((key, value) for key, value in f_depth[chro].items())
 
     for child_file in children_files:
         c_variant, var_count = get_variant_and_count(child_file)
-
         for chro in CHR:
-            if mother_file: de_novo[chro] = c_variant[chro].difference(m_variant[chro])
-            if father_file: de_novo[chro] = c_variant[chro].difference(f_variant[chro])
+            if mother_file:
+                de_novo[chro] = c_variant[chro].difference(m_variant[chro])
+            if father_file:
+                de_novo[chro] = c_variant[chro].difference(f_variant[chro])
 
-        export_vcf(child_file, de_novo, sorted_m_depth, sorted_f_depth, var_count, output_name)
+        export_vcf(child_file, de_novo, sorted_m_depth, sorted_f_depth, var_count, output_name, header)
 
 
-def export_vcf(child_file, de_novo, sorted_m_depth, sorted_f_depth, var_count, output_name):
+def export_vcf(child_file, de_novo, sorted_m_depth, sorted_f_depth, var_count, output_name, header):
     output_path = BASE_DIR + '/output/' + output_name + '.vcf'
     de_novo_count = 0
-    with open(output_path+'.vcf', 'w') as vcfFile:
-        data_writer = csv.writer(vcfFile, delimiter="\t")
+    dont_have_DP = False
+    with open(output_path, 'w') as vcfFile:
+        data_writer = csv.writer(vcfFile, delimiter="\t", lineterminator="\n", quotechar='', quoting=QUOTE_NONE)
         read_en = False
-        header_row = HEADER
-        data_writer.writerow(header_row)
-        for line in list(open(child_file, "r")):
-            words = line.strip().split()
+        rf = open(child_file, "r")
+        reader = csv.reader(rf, delimiter="\t")
+        for words in reader:
             if read_en:
-                output_row = get_all_info(words, de_novo)
+                output_row = get_all_info(words, de_novo, dont_have_DP)
                 pos = int(words[POS])
                 if len(output_row) != 0:
                     try:
@@ -63,16 +65,24 @@ def export_vcf(child_file, de_novo, sorted_m_depth, sorted_f_depth, var_count, o
                     de_novo_count += 1
             if words[CHROM] == "#CHROM":
                 read_en = True
+                if 'DP' in words:
+                    data_writer.writerow(words + header)
+                else:
+                    data_writer.writerow(words + ['DP'] + header)
+                    dont_have_DP = True
+            if read_en == False and words[CHROM].find("##") != -1:
+                data_writer.writerow(words)
         data_writer.writerow(['variant', de_novo_count, var_count])
 
 
-def get_all_info(words, de_novo):
+def get_all_info(words, de_novo, dont_have_DP):
     output_row = []
     if words[QUAL] != '.':
         chro = words[CHROM][3:]
         if int(words[POS]) in de_novo[chro]:
             output_row.extend(words)
-            output_row.append(get_dp(words[INFO], words[G]))
+            if dont_have_DP:
+                output_row.append(get_dp(words[INFO], words[G]))
     return output_row
 
 
@@ -91,7 +101,7 @@ def get_variant_and_count(filename):
     variant = dict()
     for chro in CHR:
         variant[chro] = set()
-    for line in list(open(filename, "rb")):
+    for line in list(open(filename, "r")):
         words = line.strip().split()
         if read_en and words[QUAL] != '.':
             chro = words[CHROM][3:]
@@ -128,12 +138,12 @@ def get_variant_and_depth(filename):
 def get_dp_from_dict(chro, pos, sorted_dp_dict):
     position = int(pos)
     if position in sorted_dp_dict[chro].keys():
-        print("found")
+        # print("found")
         return int(sorted_dp_dict[chro][pos])
     else:
         index = sorted_dp_dict[chro].bisect(pos)
         key = sorted_dp_dict[chro].iloc[index - 1]
-        print('not found', 'chr', chro, 'position', position, 'use', key, 'instead')
+        # print('not found', 'chr', chro, 'position', position, 'use', key, 'instead')
     return int(sorted_dp_dict[chro][key])
 
 
@@ -156,7 +166,12 @@ def get_ac(info):
 
 
 def get_name(filename):
-    start = filename.find('G')
+    start = filename.rfind('\\')
+    if start == -1:
+        start = 0
+    else:
+        start += 1
+    print(start)
     end = filename.find('.', start)
     return filename[start:end]
 
--- de_novo/views.py
+++ de_novo/views.py
@@ -5,11 +5,6 @@ from .forms import DeNovoForm
 from de_novo import de_novo
 
 
-ExAC = "D:/databases/ExAC.r0.3.1.sites.vep.csv"
-THWES154 = "D:/200_exomes/All_vcf_1607KHF-0023/154THWES_Ginger/all154.csv"
-THWES153 = "D:/200_exomes/All_vcf_1607KHF-0023/153THWES/variant.153.csv"
-
-
 def index(request):
     if request.method == 'POST':
         form = DeNovoForm(request.POST)
@@ -17,9 +12,8 @@ def index(request):
             child = form.cleaned_data['child']
             mother = form.cleaned_data['mother']
             father = form.cleaned_data['father']
-            name = form.cleaned_data['output_name'] ##***********
-
-            de_novo.de_novo([child,], mother_file=mother, father_file=father, output_name=name)
+            output_name = form.cleaned_data['output_name']
+            de_novo.de_novo([child, ], mother_file=mother, father_file=father, output_name=output_name)
             return HttpResponseRedirect('/de-novo/')
         else:
             print("invalid")
