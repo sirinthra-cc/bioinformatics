94b954a38cebfe92b18b1077ec1a408e8bfb7b79
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-02-10 12:04:13 +0700

    clean find_possible_id

--- commons/find_possible_id.py
+++ commons/find_possible_id.py
@@ -1,14 +1,13 @@
-#find all possible ID from vcf file
-#input: vcf - a vcf file
-#output: list of sample ID that appears in vcf file
+# find all possible ID from vcf file
+# input: vcf - a vcf file
+# output: list of sample ID that appears in vcf file
 
-def find_possible_ID(vcf):
+
+def find_possible_id(vcf):
     with open(vcf) as f:
         for line in f:
-            if (line[:6] == '#CHROM'):
+            if line[:6] == '#CHROM':
                 data = line.split('\t')
                 ret = data[9:]
-                ret[len(ret)-1] = ret[len(ret)-1][:-1] # remove \n from the last IDs
+                ret[len(ret)-1] = ret[len(ret)-1][:-1]  # remove \n from the last IDs
                 return ret
-                
-print(find_possible_ID("../200_exomes/G3586.vcf"))
