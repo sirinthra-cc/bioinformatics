28c1024376d6438e983b011f012a15e3ec4cf8c3
Sirinthra Chantharaj <sirinthra.cc@gmail.com>
2017-02-10 08:29:45 +0700

    set target, candidate not required

--- exomewalker/forms.py
+++ exomewalker/forms.py
@@ -4,8 +4,8 @@ from django import forms
 class ExomeWalkerForm(forms.Form):
     input = forms.CharField(label="Input file")
     entrez = forms.CharField(label='Entrez ID', widget=forms.Textarea)
-    targets = forms.CharField(label='Target List', widget=forms.Textarea)
-    candidates = forms.CharField(label='Candidate List', widget=forms.Textarea)
+    targets = forms.CharField(label='Target List', widget=forms.Textarea, required=False)
+    candidates = forms.CharField(label='Candidate List', widget=forms.Textarea, required=False)
     output_name = forms.CharField(label='Output name', max_length=100)
 
     # for ped
